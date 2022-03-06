ee290db03deca0597b81af2cf8168b4871cd55fe
Sparsh Saxena <86906056+sparsh459@users.noreply.github.com>
2022-03-06 11:55:27 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'google_api.settings')
-    try:
+    try:    
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
